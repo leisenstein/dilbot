4c7730907c7244be41e48cd9d6bcc4d75e74d406
Larry Eisenstein <larryeisenstein@gmail.com>
2016-09-16 16:57:55 -0400

    Finished default of getting today's comic.

--- app/Services.js
+++ app/Services.js
@@ -1,8 +1,11 @@
 var fs = require('fs');
 var request = require('request');
+var cheerio = require('cheerio');
+var moment = require('moment');
 
 var Services = { }
-
+Services.BaseDilbertURL = 'http://dilbert.com/';
+Services.BaseDilbertSearchURL = 'http://dilbert.com/search_results?terms=';
 
 Services.download = function(uri, filename, callback) {
     request.head(uri, function(err, resp, body) {
@@ -12,6 +15,28 @@ Services.download = function(uri, filename, callback) {
     }); 
 };
 
+Services.getToday = function(filename, callback) {
+    request(Services.BaseDilbertURL, function(error, response, html) {
+        if(error) {
+            console.log("There is an ERROR in the request (getToday)!");
+            console.log(error);
+        } else {
+            console.log("Its all good: " + response.statusCode);
+            $ = cheerio.load(html);
+            
+            var wrapped = moment(new Date());
+            var logDate = wrapped.format( "YYYY-MM-DD HH:mm").toString();
+            var todaysComicImg = $('img.img-comic').attr('src');
+            var todaysComicAlt = $('img.img-comic').attr('alt');
+            console.log(logDate + ' : ' + todaysComicImg + ' [' + todaysComicAlt +']');
+            callback(todaysComicImg);
+        }
+    });
+};
+
+Services.getRandomByTopic = function(term, uri, filename, callback) {
+    
+};
 
 Services.junk = function() {
     console.log("JUNK");
--- server.js
+++ server.js
@@ -10,28 +10,10 @@ var services = require('./app/Services');
 
 
 app.get('/scrape', function(req, res) {
-    url = 'http://dilbert.com/';
-    services.junk();
-    console.log("URL: " + url);
-    request(url, function(error, response, html) {
-        if(error) {
-            console.log("There is an ERROR in the request!");
-        } else {
-            console.log("Its all good: " + response.statusCode);
-            $ = cheerio.load(html);
-            
-            var wrapped = moment(new Date());
-            var logDate = wrapped.format( "YYYY-MM-DD HH:mm").toString();
-            
-            
-            var todaysComicImg = $('img.img-comic').attr('src');
-            var todaysComicAlt = $('img.img-comic').attr('alt');
-            console.log(logDate + ' : ' + todaysComicImg + ' [' + todaysComicAlt +']');
-            res.send(html);
-        }
-    });
-  
-    
+    services.getToday('fakefile.jpg'
+                        , function(comicImg) { 
+                            res.send('<img src="' + comicImg + '"/>');
+                        });
 });
 
 
