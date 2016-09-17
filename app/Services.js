ad0aa3898bc1bda90338cab4502036c7c744b2a7
Larry Eisenstein <larryeisenstein@gmail.com>
2016-09-17 16:46:36 -0400

    Cleanup

--- app/Services.js
+++ app/Services.js
@@ -74,8 +74,9 @@ Services.getRandomByTopic = function(term, filename, callback) {
                    var numberOfComics = $('div.img-comic-container').length;
                    
                    
-                   random = require("random-js")(1, numberOfComics);
-                   console.log('Picked Comic #: ' + random + ' of ' + numberOfComics + '!');
+                   random = require("random-js")();
+                   var randomComic = random.integer(1, numberOfComics);
+                   console.log('Picked Comic #: ' + randomComic + ' of ' + numberOfComics + '!');
                    var imgRandomComic = $('div.img-comic-container img.img-comic')[random-1];
                    var todaysComicImg = $('img.img-comic').attr('src');
                    var todaysComicAlt = $('img.img-comic').attr('alt');
